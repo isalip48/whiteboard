d07b92c624cf7255d196622338195f2233806088
isaliperera <100031700+isalip48@users.noreply.github.com>
2026-03-28 23:05:14 +0530

    able to run locally

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  allowedDevOrigins: [
+    '192.168.1.10',    // your local network IP
+    'localhost',
+  ],
 };
 
 export default nextConfig;
